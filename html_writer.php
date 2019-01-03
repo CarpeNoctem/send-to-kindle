6755164fb66859352a0f9c5fc61480402ce0ee1c
CarpeNoctem <aj2600@gmail.com>
2019-01-04 01:43:16 +1100

    pull html bits into html_writer.php

---
+++ html_writer.php
@@ -0,0 +1,17 @@
+<?
+
+function write_html($what, $content=FALSE) {
+    switch($what) {
+        case 'heading':
+            echo '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>'; break;
+        case 'footer':
+            echo '</body></html>'; break;
+        case 'link_to_file':
+            $valid_link = '<a href="' . htmlentities($content) . '">' . htmlentities($content) . '</a>';
+            file_put_contents(LINK_FILE, $valid_link);
+            break;
+        case 'success':
+            echo '( ")b &nbsp;'; break;
+    }
+}
+
--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require('config.php');
 require('user_auth.php');
 $current_user = get_user();
 
-echo '<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>';
+require('html_writer.php');
+write_html('heading');
 
 if ($current_user === 'kindle')
     include(LINK_FILE);
@@ -15,11 +16,12 @@ if ($current_user === 'kindle_admin') {
     require('valid_url.php');
     $valid_url = valid_url($_POST['input']);
     if ($valid_url) {
-        $valid_link = '<a href="' . htmlentities($valid_url) . '">' . htmlentities($valid_url) . '</a>';
-        file_put_contents(LINK_FILE, $valid_link);
-        echo '( ")b &nbsp;';
+        write_html('link_to_file', $valid_url);
+        write_html('success');
     }
     include(LINK_FILE);
 }
+
+write_html('footer');
 ?>
-</body></html>
+
