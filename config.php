2c6994d25378f8529f55e89e56e1a56ac4f659f0
CarpeNoctem <aj2600@gmail.com>
2019-01-04 00:26:46 +1100

    pull config out

---
+++ config.php
@@ -0,0 +1,5 @@
+<?
+define('LINK_FILE', 'link_eiyu87gjpSi.html');
+define('KINDLE_PASS', 'aca61a14fe58bf457a859a2d2472aa07584deed5b5f537110faa5317057d');
+define('KINDLE_ADMIN_PASS', 'fc8252c8dc55839967c58b9ad755a59b61b67c13227ddae4bd3f78a38bf39');
+define('COOKIE_NAME', 'iye989n7KpGJLaTGdsrFv');
